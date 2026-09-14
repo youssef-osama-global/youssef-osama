42bb844dff1dd88e7e65e5331632021071c3c88f
youssef-osama-global <youssef.osama.global@gmail.com>
2026-09-14 15:42:58 +0200

    file organization

--- src/components/hero_sec/hero-content/hero-content.jsx
+++ src/components/hero_sec/hero-content/hero-content.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import './hero-content.css'
-import NavBar from "./nav-bar/nav-bar"
+import NavBar from "../../nav-bar/nav-bar"
 
 function HeroContent() {
   return (
